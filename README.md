97f6d1d6a12595574065813ae5cc28856891d120
Boris Kotov <bk@webdeb.de>
2023-06-30 03:03:47 +0200

    Add Readme

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Plusminus.club website
+
+### Rules for contribute
+
+1. Use mdx format for pages
+
+```mdx
+---
+title: The TRIAC
+description: Here you will learn what the triac is, how it works, and which problems it solves.
+---
+
+### The Triac
+
+...
+```
+
+2. Use `<MathBlock />` Component for Math expressions
+
+```mdx
+import MathBlock from "@components/MathBlock";
+
+### Some formulas
+
+<MathBlock
+  description="Quadratic function"
+  math="y = x*x"
+  // or for more precise latex expressions
+  latex="f(x) = x \cdot x"
+  explanation="x gets multiplied by itsef."
+/>
+
+The formula above is just an example.. ..
+```
+
+3. Add illustrations, but use only open source images.
+
+4. Use the Simulator Module
--- theme.config.tsx
+++ theme.config.tsx
@@ -9,7 +9,7 @@ const config: DocsThemeConfig = {
   chat: {
     link: "https://t.me",
     icon: () => (
-      <svg xmlns="http://www.w3.org/2000/svg" width="30 " viewBox="0 0 48 48">
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" viewBox="0 0 48 48">
         <path
           fill="currentColor"
           d="M41.4193 7.30899s3.8853-1.515 3.5615 2.16429c-.1079 1.51502-1.0792 6.81752-1.8347 12.55292l-2.5902 16.9897s-.2158 2.4889-2.1585 2.9218c-1.9427.4328-4.8566-1.515-5.3963-1.9479-.4317-.3246-8.0943-5.1943-10.7925-7.575-.7555-.6493-1.6189-1.9479.1079-3.4629l11.3322-10.8214c1.2951-1.2986 2.5902-4.3286-2.8061-.6493L15.7331 27.7616s-1.7268 1.0821-4.9645.1082l-7.01518-2.1643s-2.59021-1.6232 1.83473-3.2465C16.3807 17.3729 29.6555 12.1786 41.4193 7.30899Z"
@@ -44,7 +44,10 @@ const config: DocsThemeConfig = {
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:title" content="plusminus.club" />
-      <meta property="og:description" content="Site about electicity" />
+      <meta
+        property="og:description"
+        content="Site about electicity research"
+      />
       <link rel="icon" href="/favicon.png" type="image/png" />
     </>
   ),
