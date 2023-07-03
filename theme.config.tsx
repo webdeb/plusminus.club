0a57fa8384bdcfa28ce9c360c113f36b3b39d25e
Boris Kotov <bk@webdeb.de>
2023-07-04 00:30:44 +0200

    Add tg link

--- theme.config.tsx
+++ theme.config.tsx
@@ -7,7 +7,7 @@ const config: DocsThemeConfig = {
   logo: Logo,
   darkMode: false,
   chat: {
-    link: "https://t.me",
+    link: "https://t.me/plusminusclub",
     icon: () => (
       <svg xmlns="http://www.w3.org/2000/svg" width="30" viewBox="0 0 48 48">
         <path
