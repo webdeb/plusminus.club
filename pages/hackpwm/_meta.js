a30c47c92e617de3ba3dab0c27ac215b7c3d6692
Boris Kotov <bk@webdeb.de>
2024-02-23 21:32:47 +0100

    Add hackpwm page

---
+++ pages/hackpwm/_meta.js
@@ -0,0 +1,14 @@
+export default {
+  "*": {
+    type: "doc",
+    theme: {
+      layout: "default",
+    },
+  },
+  index: {
+    title: "Basic Info",
+    theme: {
+      breadcrumb: false,
+    },
+  },
+};
